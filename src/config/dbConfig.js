2c6222d29ae3b4710e974a96e3952e51f725e292
pamratchadaphon <139990701+pamratchadaphon@users.noreply.github.com>
2024-07-27 17:13:35 +0700

    updated

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,6 +1,7 @@
 module.exports = {
   URL: 'mysql://4NJC5b5VMVQsHTn.root:9LQiumfAD6WtmGVM@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/RiceCultivationByFarmers',
   dialect: 'mysql',
+  dialectModule: require('mysql2'), 
   dialectOptions: {
     ssl: {
       rejectUnauthorized: true
