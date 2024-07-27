57ab088ab6ca6f128627bb872ffec2006d5834cd
pamratchadaphon <139990701+pamratchadaphon@users.noreply.github.com>
2024-07-27 18:23:09 +0700

    update

--- index.js
+++ index.js
@@ -31,12 +31,4 @@ app.use('/Images', express.static('./Images'))
 const routerNewsService = require('./src/routes/newsServiceRoute');
 app.use('/newsService', routerNewsService)
 
-// const port = 8080;
-
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(port, () => {
-//     console.log("server running on port " + port);
-//   });
-// });
-
 module.exports = app;
