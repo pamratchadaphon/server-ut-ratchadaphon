e244c5adb2e992bf9176ee8adcc31e2b2a72326a
pamratchadaphon <139990701+pamratchadaphon@users.noreply.github.com>
2024-07-28 16:07:18 +0700

    update vercel.json

--- index.js
+++ index.js
@@ -8,38 +8,45 @@ const jsonParser = bodyParser.json();
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  credentials: true,
-  origin: 'http://localhost:5173' 
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('This is API running')
-})
+app.get("/", (req, res) => {
+  res.send("This is API running");
+});
 
-const routeUser = require('./src/routes/userRoute')
-app.use('/user', routeUser)
+const routeUser = require("./src/routes/userRoute");
+app.use("/user", routeUser);
 
-const routeRiceVariety = require('./src/routes/riceVarietyRoute')
-app.use('/riceVariety', routeRiceVariety)
+const routeRiceVariety = require("./src/routes/riceVarietyRoute");
+app.use("/riceVariety", routeRiceVariety);
 
-const routeRiceCaltivation = require('./src/routes/riceCaltivationRoute')
-app.use('/riceCaltivation', routeRiceCaltivation)
+const routeRiceCaltivation = require("./src/routes/riceCaltivationRoute");
+app.use("/riceCaltivation", routeRiceCaltivation);
 
-const routerIncomeExpense = require('./src/routes/incomeExpenseRoute');
-app.use('/incomeExpense', routerIncomeExpense)
+const routerIncomeExpense = require("./src/routes/incomeExpenseRoute");
+app.use("/incomeExpense", routerIncomeExpense);
 
-app.use('/Images', express.static('./Images'))
+app.use("/Images", express.static("./Images"));
 
-const routerNewsService = require('./src/routes/newsServiceRoute');
-app.use('/newsService', routerNewsService)
+const routerNewsService = require("./src/routes/newsServiceRoute");
+app.use("/newsService", routerNewsService);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 module.exports = app;
