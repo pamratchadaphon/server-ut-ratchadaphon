0a0d21bdb0288a25a6e43c13887bdf5612771956
pamratchadaphon <139990701+pamratchadaphon@users.noreply.github.com>
2024-07-31 00:51:28 +0700

    update

--- src/controllers/riceVarietyController.js
+++ src/controllers/riceVarietyController.js
@@ -1,127 +1,10 @@
-// const db = require("../models");
-// const multer = require("multer");
-// const path = require("path");
-
-// const RiceVariety = db.RiceVariety;
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "Images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
-// });
-
-// const upload = multer({
-//   storage: storage,
-//   limits: { fileSize: "1000000" },
-//   fileFilter: (req, file, cb) => {
-//     const fileTypes = /jpeg|jpg|png|gif/;
-//     const mimeType = fileTypes.test(file.mimetype);
-//     const extname = fileTypes.test(path.extname(file.originalname));
-
-//     if (mimeType && extname) {
-//       return cb(null, true);
-//     }
-//     cb("Give proper files formate to upload");
-//   },
-// }).single("image");
-
-// module.exports = {
-//   async create(req, res) {
-//     const info = {
-//       precautions: req.body.precautions,
-//       photosensitivity: req.body.photosensitivity,
-//       water_requirement: req.body.water_requirement,
-//       yield: req.body.yield,
-//       name: req.body.name,
-//       stability: req.body.stability,
-//       height: req.body.height,
-//       feature: req.body.feature,
-//       age: req.body.age,
-//       type: req.body.type,
-//       image: req.file.path,
-//     };
-//     const newRiceVariety = await RiceVariety.create(info);
-//     res.status(201).send(newRiceVariety);
-//   },
-//   async index(req, res) {
-//     const riceVariety = await RiceVariety.findAll();
-//     res.status(200).send(riceVariety);
-//   },
-//   async show(req, res) {
-//     const riceVariety = await RiceVariety.findOne({
-//       where: { riceVariety_id: req.params.riceVariety_id },
-//     });
-//     res.status(200).send(riceVariety);
-//   },
-//   async update1(req, res) {
-//     const info = {
-//       precautions: req.body.precautions,
-//       photosensitivity: req.body.photosensitivity,
-//       water_requirement: req.body.water_requirement,
-//       yield: req.body.yield,
-//       name: req.body.name,
-//       stability: req.body.stability,
-//       height: req.body.height,
-//       feature: req.body.feature,
-//       age: req.body.age,
-//       type: req.body.type,
-//       image: req.file.path,
-//     };
-//     await RiceVariety.update(info, {
-//       where: { riceVariety_id: req.params.riceVariety_id },
-//     });
-//     res.status(200).send(info);
-//   },
-//   async update2(req, res) {
-//     const info = {
-//       precautions: req.body.precautions,
-//       photosensitivity: req.body.photosensitivity,
-//       water_requirement: req.body.water_requirement,
-//       yield: req.body.yield,
-//       name: req.body.name,
-//       stability: req.body.stability,
-//       height: req.body.height,
-//       feature: req.body.feature,
-//       age: req.body.age,
-//       type: req.body.type
-//     };
-//     await RiceVariety.update(info, {
-//       where: { riceVariety_id: req.params.riceVariety_id },
-//     });
-//     res.status(200).send(info);
-//   },
-//   async delete(req, res) {
-//     await RiceVariety.destroy({
-//       where: { riceVariety_id: req.params.riceVariety_id },
-//     });
-//     res.status(200).send("riceVariety is deleted!");
-//   },
-//   upload,
-// };
 const db = require("../models");
 const RiceVariety = db.RiceVariety;
 
 module.exports = {
   async create(req, res) {
     try {
-      const info = {
-        precautions: req.body.precautions,
-        photosensitivity: req.body.photosensitivity,
-        water_requirement: req.body.water_requirement,
-        yield: req.body.yield,
-        name: req.body.name,
-        stability: req.body.stability,
-        height: req.body.height,
-        feature: req.body.feature,
-        age: req.body.age,
-        type: req.body.type,
-        image: req.body.image, // รับลิงก์รูปภาพจาก req.body
-      };
-
-      const newRiceVariety = await RiceVariety.create(info);
+      const newRiceVariety = await RiceVariety.create(req.body);
       res.status(201).send(newRiceVariety);
     } catch (error) {
       res.status(500).send({ message: "Error creating rice variety", error });
@@ -154,24 +37,17 @@ module.exports = {
 
   async update1(req, res) {
     try {
-      const info = {
-        precautions: req.body.precautions,
-        photosensitivity: req.body.photosensitivity,
-        water_requirement: req.body.water_requirement,
-        yield: req.body.yield,
-        name: req.body.name,
-        stability: req.body.stability,
-        height: req.body.height,
-        feature: req.body.feature,
-        age: req.body.age,
-        type: req.body.type,
-        image: req.body.image, // รับลิงก์รูปภาพจาก req.body
-      };
-
-      await RiceVariety.update(info, {
+      const [updated] = await RiceVariety.update(req.body, {
         where: { riceVariety_id: req.params.riceVariety_id },
       });
-      res.status(200).send(info);
+      if (updated) {
+        const updatedRiceVariety = await RiceVariety.findOne({
+          where: { riceVariety_id: req.params.riceVariety_id },
+        });
+        res.status(200).send(updatedRiceVariety);
+      } else {
+        res.status(404).send({ message: "Rice variety not found for update" });
+      }
     } catch (error) {
       res.status(500).send({ message: "Error updating rice variety", error });
     }
@@ -192,10 +68,17 @@ module.exports = {
         type: req.body.type,
       };
 
-      await RiceVariety.update(info, {
+      const [updated] = await RiceVariety.update(info, {
         where: { riceVariety_id: req.params.riceVariety_id },
       });
-      res.status(200).send(info);
+      if (updated) {
+        const updatedRiceVariety = await RiceVariety.findOne({
+          where: { riceVariety_id: req.params.riceVariety_id },
+        });
+        res.status(200).send(updatedRiceVariety);
+      } else {
+        res.status(404).send({ message: "Rice variety not found for update" });
+      }
     } catch (error) {
       res.status(500).send({ message: "Error updating rice variety", error });
     }
@@ -203,10 +86,14 @@ module.exports = {
 
   async delete(req, res) {
     try {
-      await RiceVariety.destroy({
+      const deleted = await RiceVariety.destroy({
         where: { riceVariety_id: req.params.riceVariety_id },
       });
-      res.status(200).send("Rice variety is deleted!");
+      if (deleted) {
+        res.status(200).send("Rice variety is deleted!");
+      } else {
+        res.status(404).send({ message: "Rice variety not found for deletion" });
+      }
     } catch (error) {
       res.status(500).send({ message: "Error deleting rice variety", error });
     }
